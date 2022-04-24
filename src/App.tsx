05665480badee8d8426588aa7bfe8d5067e2e320
LukeHeimen <kdlanante@up.edu.ph>
2022-04-24 23:00:12 +0800

    Initial repo setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.scss";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <h1>Hello!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
